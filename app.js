936d32d222041bd5b85a04324be7f5a5b419967f
NamWoohyun <namwhis@gmail.com>
2025-04-09 17:26:22 +0900

    mousemove-painting

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-//ctx.fillRect(50, 50, 100, 200);
-ctx.rect(50,50,100,100);
-ctx.fill();
+ctx.lineWidth =2;
+ctx.moveTo(0,0);
 
-ctx.beginPath();
-ctx.rect(150,150,100,100);
-ctx.rect(250,250,100,100);
-ctx.fillStyle = "red";
-ctx.fill();
+const colors = [
+    "#f6e58d",
+    "#f6e58d",
+    "#ff7979",
+    "#badc58",
+    "#dff9fb",
+    "#f9ca24",
+    "#f0932b",
+    "#6ab04c",
+    "#c7ecee",
+    "#7ed6df",
+    "#e056fd",
+    "#686de0"
+]
+
+function onMouseMove(event){
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    const color = colors[Math.floor(Math.random()* colors.length)];
+    ctx.strokeStyle=color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onMouseMove);
 
